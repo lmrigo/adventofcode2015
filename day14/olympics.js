6b691634d822839c0992ff9e2ad80f1e0da8c3a8
Lucas Rigo <lucas.rigo@sap.com>
2017-02-13 10:11:01 -0200

    implemented day 14 part 2 solution

--- day14/olympics.js
+++ day14/olympics.js
@@ -5,12 +5,12 @@ Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`,
   puzzleInput
 ]
 
-var reindeers
+
 
 var day14 = function() {
 
   for (var i = 0; i < input.length; i++) {
-    reindeers = []
+    var reindeers = []
 
     var inLines = input[i].split(/\n/)
     for (var j = 0; j < inLines.length; j++) {
@@ -63,10 +63,69 @@ var day14 = function() {
 var day14part2 = function() {
 
 
-  for (var i = 7; i < input.length; i++) {
+  for (var i = 0; i < input.length; i++) {
+    var reindeers = []
+
+    var inLines = input[i].split(/\n/)
+    for (var j = 0; j < inLines.length; j++) {
+      var inDeer = inLines[j].split(/\s/)
+      var deer = {
+        name: inDeer[0],
+        speed: Number(inDeer[3]),
+        stamina: Number(inDeer[6]),
+        rest: Number(inDeer[13]),
+        position: 0,
+        state: 'awake',
+        timer: Number(inDeer[6]) // stamina
+      }
+      reindeers.push(deer)
+    }
+    // console.log(reindeers)
+
+
+    var points = {}
+    for (var d = 0; d < reindeers.length; d++) {
+      points[reindeers[d].name] = 0
+    }
+
+    // race
+    var seconds = 2503
+    while (seconds--) {
+      for (var d = 0; d < reindeers.length; d++) {
+        var deer = reindeers[d]
+        if (deer.state === 'awake') {
+          deer.position += deer.speed
+          if (!(--deer.timer)) {
+            deer.state = 'sleep'
+            deer.timer = deer.rest
+          }
+        } else {
+          if (!(--deer.timer)) {
+            deer.state = 'awake'
+            deer.timer = deer.stamina
+          }
+        }
+      }
+      // find winning position
+      var maxPos = reindeers.reduce(function(acc, val) {
+        return (acc > val.position ? acc : val.position)
+      }, 0)
+      // assign points
+      for (var d = 0; d < reindeers.length; d++) {
+        if (reindeers[d].position === maxPos) {
+          points[reindeers[d].name]++
+        }
+      }
+    }
+
+    var winner = Object.keys(points).reduce(function(acc, val) {
+      return (acc > points[val] ? acc : points[val])
+    }, 0)
+    // console.log(winner)
+
     $('#day14part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(winner)
       .append('<br>')
   }
 

cbb2f60ca318fca5cb5c8a1c3571475a30643eef
Lucas Rigo <lucas.rigo@sap.com>
2017-02-17 17:39:23 -0200

    implmement day 16 part 2 solution

--- day16/auntSue.js
+++ day16/auntSue.js
@@ -46,6 +46,36 @@ var day16 = function() {
     }
     // console.log(aunts)
 
+    var childrenFilter = function(aunt) {
+      return aunt.children === children || aunt.children === undefined
+    }
+    var catsFilter = function(aunt) {
+      return aunt.cats === cats || aunt.cats === undefined
+    }
+    var samoyedsFilter = function(aunt) {
+      return aunt.samoyeds === samoyeds || aunt.samoyeds === undefined
+    }
+    var pomeraniansFilter = function(aunt) {
+      return aunt.pomeranians === pomeranians || aunt.pomeranians === undefined
+    }
+    var akitasFilter = function(aunt) {
+      return aunt.akitas === akitas || aunt.akitas === undefined
+    }
+    var vizslasFilter = function(aunt) {
+      return aunt.vizslas === vizslas || aunt.vizslas === undefined
+    }
+    var goldfishFilter = function(aunt) {
+      return aunt.goldfish === goldfish || aunt.goldfish === undefined
+    }
+    var treesFilter = function(aunt) {
+      return aunt.trees === trees || aunt.trees === undefined
+    }
+    var carsFilter = function(aunt) {
+      return aunt.cars === cars || aunt.cars === undefined
+    }
+    var perfumesFilter = function(aunt) {
+      return aunt.perfumes === perfumes || aunt.perfumes === undefined
+    }
 
     var filtered = aunts.filter(childrenFilter)
     filtered = filtered.filter(catsFilter)
@@ -69,45 +99,74 @@ var day16 = function() {
   }
 }
 
-var childrenFilter = function(aunt) {
-  return aunt.children === children || aunt.children === undefined
-}
-var catsFilter = function(aunt) {
-  return aunt.cats === cats || aunt.cats === undefined
-}
-var samoyedsFilter = function(aunt) {
-  return aunt.samoyeds === samoyeds || aunt.samoyeds === undefined
-}
-var pomeraniansFilter = function(aunt) {
-  return aunt.pomeranians === pomeranians || aunt.pomeranians === undefined
-}
-var akitasFilter = function(aunt) {
-  return aunt.akitas === akitas || aunt.akitas === undefined
-}
-var vizslasFilter = function(aunt) {
-  return aunt.vizslas === vizslas || aunt.vizslas === undefined
-}
-var goldfishFilter = function(aunt) {
-  return aunt.goldfish === goldfish || aunt.goldfish === undefined
-}
-var treesFilter = function(aunt) {
-  return aunt.trees === trees || aunt.trees === undefined
-}
-var carsFilter = function(aunt) {
-  return aunt.cars === cars || aunt.cars === undefined
-}
-var perfumesFilter = function(aunt) {
-  return aunt.perfumes === perfumes || aunt.perfumes === undefined
-}
-
 var day16part2 = function() {
 
 
   for (var i = 0; i < input.length; i++) {
+    aunts = []
+
+    var inLines = input[i].split(/\n/)
+    for (var j = 0; j < inLines.length; j++) {
+      var sue = inLines[j].split(/\s/)
+      //0   1  2     3  4     5  6     7
+      //Sue #: obj1: #, obj2: #, obj3: #
+      var num = Number(sue[1].replace(':',''))
+      var aunt = new Aunt(num)
+      aunt[sue[2].replace(':','')] = Number(sue[3].replace(',',''))
+      aunt[sue[4].replace(':','')] = Number(sue[5].replace(',',''))
+      aunt[sue[6].replace(':','')] = Number(sue[7])
+      aunts[num] = aunt
+    }
+    // console.log(aunts)
+
+    var childrenFilter = function(aunt) {
+      return aunt.children === children || aunt.children === undefined
+    }
+    var catsFilter = function(aunt) {
+      return aunt.cats > cats || aunt.cats === undefined
+    }
+    var samoyedsFilter = function(aunt) {
+      return aunt.samoyeds === samoyeds || aunt.samoyeds === undefined
+    }
+    var pomeraniansFilter = function(aunt) {
+      return aunt.pomeranians < pomeranians || aunt.pomeranians === undefined
+    }
+    var akitasFilter = function(aunt) {
+      return aunt.akitas === akitas || aunt.akitas === undefined
+    }
+    var vizslasFilter = function(aunt) {
+      return aunt.vizslas === vizslas || aunt.vizslas === undefined
+    }
+    var goldfishFilter = function(aunt) {
+      return aunt.goldfish < goldfish || aunt.goldfish === undefined
+    }
+    var treesFilter = function(aunt) {
+      return aunt.trees > trees || aunt.trees === undefined
+    }
+    var carsFilter = function(aunt) {
+      return aunt.cars === cars || aunt.cars === undefined
+    }
+    var perfumesFilter = function(aunt) {
+      return aunt.perfumes === perfumes || aunt.perfumes === undefined
+    }
+
+    var filtered = aunts.filter(childrenFilter)
+    filtered = filtered.filter(catsFilter)
+    filtered = filtered.filter(samoyedsFilter)
+    filtered = filtered.filter(pomeraniansFilter)
+    filtered = filtered.filter(akitasFilter)
+    filtered = filtered.filter(vizslasFilter)
+    filtered = filtered.filter(goldfishFilter)
+    filtered = filtered.filter(treesFilter)
+    filtered = filtered.filter(carsFilter)
+    filtered = filtered.filter(perfumesFilter)
+    // console.log(filtered.length)
+
+    var auntNumber = filtered[0].num
 
     $('#day16part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(auntNumber)
       .append('<br>')
   }
 

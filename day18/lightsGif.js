62ec3155c9300dfcf52ceb284edc8e1222c0e528
Lucas Rigo <lucas.rigo@sap.com>
2017-02-21 13:27:13 -0300

    implmented day 18 part 2 solution

--- day18/lightsGif.js
+++ day18/lightsGif.js
@@ -71,13 +71,11 @@ var turnOff = function (tlX, tlY, bdX, bdY) {
   }
 }
 
-var gridSize = 100
-
 var day18 = function() {
 
   for (var i = 0; i < input.length; i++) {
     var totalSteps = i === 0 ? 4 : 100
-    gridSize = i === 0 ? 6 : 100
+    var gridSize = i === 0 ? 6 : 100
 
     //reset grid
     grid = []
@@ -126,8 +124,8 @@ var day18 = function() {
       .append('<br>&emsp;')
       .append(litLights)
       .append('<br>')
-      .append(printGrid())
-      .append('<br>')
+      // .append(printGrid())
+      // .append('<br>')
   }
 }
 
@@ -135,19 +133,70 @@ var day18 = function() {
 var day18part2 = function() {
 
   for (var i = 0; i < input.length; i++) {
+    var totalSteps = i === 0 ? 5 : 100
+    var gridSize = i === 0 ? 6 : 100
+
     //reset grid
     grid = []
     for (var gi = 0; gi < gridSize; gi++) {
       grid[gi] = []
       for (var gj = 0; gj < gridSize; gj++) {
-        grid[gi][gj] = 0
+        grid[gi][gj] = false
       }
     }
 
+    // initialize grid with input
+    var rows = input[i].split(/\n/)
+    for (var r = 0; r < rows.length; r++) {
+      var cols = rows[r].split('')
+      for (var c = 0; c < cols.length; c++) {
+        grid[r][c] = (cols[c] === '#')
+      }
+    }
+
+    // set corners on
+    grid[0][0] = true
+    grid[0][gridSize-1] = true
+    grid[gridSize-1][0] = true
+    grid[gridSize-1][gridSize-1] = true
+
+    // run animation
+    var steps = totalSteps
+    while (steps--) {
+      var newGrid = copyGrid()
+      // generate next state
+      for (var gi = 0; gi < grid.length; gi++) {
+        for (var gj = 0; gj < grid[gi].length; gj++) {
+          var on = newGrid[gi][gj]
+          var nighbours = countLitNeighbours(gi, gj)
+          if (on) {
+            newGrid[gi][gj] = (nighbours === 2 || nighbours === 3) ? true : false
+          } else {
+            newGrid[gi][gj] = (nighbours === 3) ? true : false
+          }
+        }
+      }
+      grid = newGrid
+      // set corners on
+      grid[0][0] = true
+      grid[0][gridSize-1] = true
+      grid[gridSize-1][0] = true
+      grid[gridSize-1][gridSize-1] = true
+      // console.log(printGrid())
+    }
+
+    var litLights = grid.reduce(function(accum1, row) {
+      return accum1 + row.reduce(function(accum2, val) {
+        return accum2 + (val ? 1 : 0)
+      }, 0)
+    }, 0)
+
     $('#day18part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(litLights)
       .append('<br>')
+      // .append(printGrid())
+      // .append('<br>')
   }
 
 }

fb525cf12bee04200f2ac1b8c1e97d742b680ca9
Lucas Rigo <lucas.rigo@sap.com>
2017-03-29 17:28:25 -0300

    cleaned up and improved day 24 part 1

--- day24/balance.js
+++ day24/balance.js
@@ -104,6 +104,7 @@ var ht = { // history tree
   }
 }
 
+// not needed, but useful to see some results
 var findTheOther2 = function (g1, thirdWeight) {
   var remn = weights.filter((x) => {
     return !g1.includes(x)
@@ -176,32 +177,38 @@ var day24 = function() {
     // console.log(thirdWeight)
     // 20, 512
 
-    weights.reverse(s)
+    // weights.reverse(s) // goes slower reversed as the algorithm "pops". If it "shifted" it would be better.
 
+    var finalGroups = []
+    var minLength = 1000
     var initial = {'wrem': thirdWeight, 'lst': []}
     var queue = [initial]
-    var counter = 1, treeCounter = 0
+    // var counter = 1, treeCounter = 0
     while (queue.length > 0) {
       // var grp = queue.shift()
       var grp = queue.pop()
-      if (grp.lst.length < 6) {
+      if (grp.lst.length < minLength) {
         $.each(weights, (idx, w) => {
           if (!grp.lst.includes(w)) {
-            if ((counter++ % 100000000) === 0) {
-              counter = 1
-              console.log(grp.lst.join(','))
-            }
+            // if ((counter++ % 100000000) === 0) {
+            //   counter = 1
+            //   console.log(grp.lst.join(','))
+            // }
             var newGrp = {'wrem': (grp.wrem - w), 'lst': []}
             if (newGrp.wrem >= 0) {
               newGrp.lst.push(...grp.lst)
               newGrp.lst.insertSorted(w)
-              if (newGrp.wrem === 0 && newGrp.lst.length <= 6) {
+              if (newGrp.wrem === 0 && newGrp.lst.length <= minLength) {
+                if (newGrp.lst.length < minLength) {
+                  minLength = newGrp.lst.length
+                }
                 if (!ht.includes(ht.root, newGrp.lst, 0)) {
                   // findTheOther2(newGrp.lst, thirdWeight)
+                  finalGroups.push(newGrp.lst.slice())
                   ht.insert(ht.root, newGrp.lst)
-                  if (treeCounter++ % 100 === 0) {
-                    console.log(ht.toString())
-                  }
+                  // if (treeCounter++ % 100 === 0) {
+                  //   console.log(ht.toString())
+                  // }
                 }
               } else {
                 queue.push(newGrp)
@@ -212,14 +219,18 @@ var day24 = function() {
       }
     }
 
-    // console.log(ht.toString())
-
-    //divide the weights in three groups of same weight
+    var shorts = finalGroups.filter((val) => {
+      return val.length <= minLength
+    })
+    var smallestQE = shorts.reduce((acc, val) => {
+      return quantumEntanglement(acc) < quantumEntanglement(val) ? acc : val
+    })
+    console.log(smallestQE, quantumEntanglement(smallestQE))
 
 
     $('#day24').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(quantumEntanglement(smallestQE))
       .append('<br>')
   }
 }
@@ -242,197 +253,3 @@ $(function (){
   day24part2()
   $('#main').append('<br>')
 })
-
-/*
-
-    // all groups with thirdWeight
-    var initGrps = weights.map((w) => {
-      return new Group(w)
-    })
-    var allGroups = generateThirdGroups(initGrps, weights, thirdWeight, [])
-
-    // console.log(allGroups) // 2474=length
-    var str = ''
-    $.each(allGroups, (idx, val) => {
-      str += val.toString() + ', '
-    })
-    console.log(str)
-
-
-
-function Group(weight) {
-  var p = weight !== undefined
-  this.weights = p ? [weight] : []
-  this.sum = p ? weight : 0
-  this.signature = this.weights.join('')+'|'+this.sum
-  this.addWeight = function (w) {
-    this.weights.push(w)
-    this.weights.sort(s)
-    this.sum += w
-    this.signature = this.weights.join('')+'|'+this.sum
-  }
-  this.containsWeight = function (w) {
-    return this.weights.includes(w)
-  }
-  this.putWeight = function (w) {
-    if (!this.containsWeight(w)) {
-      this.addWeight(w)
-    }
-  }
-  this.clone = function () {
-    var c = new Group()
-    c.weights.push(...this.weights)
-    c.sum = this.sum
-    c.weights.sort(s)
-    c.signature = c.weights.join('')+'|'+c.sum
-    return c
-  }
-  this.equals = function (other) {
-    if (this.sum !== other.sum || this.weights.length !== other.weights.length) {
-      return false
-    } else {
-      // return this.weights.join('') === other.weights.join('')
-      return  this.signature === other.signature
-    }
-  }
-  this.toString = function () {
-    return `[${this.weights.join(',')}]`
-  }
-}
-
-var generateThirdGroups = function (grps, ws, thirdWeight, thirdGroups) {
-  if (grps.length === 0) {
-    return thirdGroups
-  } else {
-    // var newGrps = []
-    $.each(grps, (ig, g) => {
-      if (g.sum < thirdWeight) {
-        $.each(ws, (iw, w) => {
-          if (!g.containsWeight(w)) {
-            var ng = g.clone()
-            ng.addWeight(w)
-            if (ng.sum === thirdWeight) {
-              if (!thirdGroups.find((e) => {
-                return e.equals(ng)
-              })) {
-                thirdGroups.push(ng)
-                console.log(thirdGroups.length)
-                if (thirdGroups.length % 100 === 0) {
-                  console.log(ng.toString())
-                }
-                if (thirdGroups.length % 200 === 0) {
-                  console.log(thirdGroups.reduce((acc, val) => { return acc + ', ' + val.toString()} ))
-                }
-              }
-            } else if (ng.sum < thirdWeight) {
-              generateThirdGroups([ng], ws, thirdWeight, thirdGroups)
-              // newGrps.push(ng)
-            }
-          }
-        })
-      } else  {
-        console.log('I should not get here!')
-      }
-    })
-    return thirdGroups
-    // return generateThirdGroups(newGrps, ws, thirdWeight, thirdGroups)
-  }
-}
-
-*/
-
-
-
-/*
-
-    var thirdGroups = []
-    var isNewGroup = function (newGrp) {
-      var isNew = true
-      for (var g = 0; g < thirdGroups.length; g++) {
-        if (arrayEquals(thirdGroups[g], newGrp)) {
-          isNew = false
-          break
-        }
-      }
-      return isNew
-    }
-    var triplets = []
-
-    var queue = weights.map(function(val) {
-      return [val]
-    })
-    var counter = 0
-    while (queue.length > 0) {
-      if (counter++ % 100000000 === 0) {
-        console.log('q: '+queue.length, thirdGroups.length)
-      }
-      // var grp = queue.shift()
-      var grp = queue.pop()
-      var sum = grp.reduce(function(acc, val) {
-        return acc + val
-      })
-      if (sum > thirdWeight) {
-        continue
-      } else if (sum === thirdWeight) {
-        grp.sort(s)
-        if (isNewGroup(grp)) {
-          thirdGroups.push(grp)
-
-          var remn = weights.filter(function (val) {
-            return !grp.includes(val)
-          })
-          var q2 = remn.map(function(val) {
-            return [val]
-          })
-          while (q2.length > 0) {
-            var g2 = q2.shift()
-            var sum2 = g2.reduce(function(acc, val) {
-              return acc + val
-            })
-            if (sum2 > thirdWeight) {
-              continue
-            } else if (sum2 === thirdWeight) {
-              g2.sort(s)
-              if (isNewGroup(g2)) {
-                thirdGroups.push(g2)
-                var g3 = weights.filter(function (val) {
-                  return !grp.includes(val) && !g2.includes(val)
-                })
-                var sum3 = g3.reduce(function(acc, val) {
-                  return acc + val
-                })
-                if (sum3 !== thirdWeight) {
-                  continue
-                } else {
-                  g3.sort(s)
-                  if (isNewGroup(g3)) {
-                    thirdGroups.push(g3)
-                    triplets.push([grp, g2, g3])
-                  }
-                }
-              }
-            } else {
-              for (var w = 0; w < weights.length; w++) {
-                if (!g2.includes(weights[w])) {
-                  var cpy = g2.slice()
-                  cpy.push(weights[w])
-                  q2.push(cpy)
-                }
-              }
-            }
-          }
-        }
-      } else {
-        for (var w = 0; w < weights.length; w++) {
-          if (!grp.includes(weights[w])) {
-            var cpy = grp.slice()
-            cpy.push(weights[w])
-            queue.push(cpy)
-          }
-        }
-      }
-    }
-    // console.log(thirdGroups)
-    console.log(thirdGroupsToString() + thirdGroups.length)
-    console.log(triplets)
-*/
